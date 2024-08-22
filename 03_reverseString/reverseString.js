a41b60964c61145c1c8f588ebeb23c372b56745a
Jonas Schweiger <jonas.schweiger91@gmail.com>
2024-08-22 08:52:19 +0200

    Finish exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    // let strArr = string.split('');
+    // let reverseStrArr = [];
+    // let j = 0;
 
+    // for (let i = strArr.length - 1; i >= 0; i--) {
+    //     reverseStrArr[j] = strArr[i];
+    //     j++;
+    // }
+
+    // return reverseStrArr.join('');
+
+    return string.split('').reverse().join('');
 };
 
 // Do not edit below this line
