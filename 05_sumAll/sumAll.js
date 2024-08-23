599748c2cc1cc868efbecbf97d2aee2150450bd1
Jonas Schweiger <jonas.schweiger91@gmail.com>
2024-08-23 08:55:39 +0200

    Finish exercise 06

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,23 +1,25 @@
-const sumAll = function(num1, num2) {
-    if (num1 < 0 || num2 < 0) {
+const sumAll = function(min, max) {
+    if (min < 0 || max < 0) {
         return 'ERROR';
     }
-    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
-        return 'ERROR';
-    }
-    if (Math.floor(num1) !== num1 || Math.floor(num2) !== num2) {
+    // if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+    //     return 'ERROR';
+    // }
+    // if (Math.floor(num1) !== num1 || Math.floor(num2) !== num2) {
+    //     return 'ERROR';
+    // }
+    if (!Number.isInteger(min) || !Number.isInteger(max)) {
         return 'ERROR';
     }
 
-    let smallNum = num1;
-    let bigNum = num2;
+    if (min > max) {
+        const temp = min;
+        min = max;
+        max = temp;
+    }
+
     let sum = 0;
-
-    if (num1 > num2) {
-        smallNum = num2;
-        bigNum = num1;
-    } 
-    for (let i = smallNum;  i <= bigNum; i++) {
+    for (let i = min;  i <= max; i++) {
         sum += i;
     }
     return sum;
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // declare constants for better readability
+    const isDivisibleByFour = year % 4 === 0;
+    const isCentury = year % 100 === 0;
+    const isDivisibleByFourHundred = year % 400 === 0;
+    let isLeapYear = false;
+    
+    if (isDivisibleByFour && !isCentury || isDivisibleByFourHundred) {
+        isLeapYear = true;
+    }    
 
+    return isLeapYear;
 };
 
 // Do not edit below this line
